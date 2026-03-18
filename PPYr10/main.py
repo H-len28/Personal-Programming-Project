4104b32c035c59a54d364e589a3d36818265583e
Helen <Hmccreery28@kellettschool.com>
2026-03-18 12:50:08 +0800

    I started on the merging of the blocks and added colour to the main menu

--- PPYr10/main.py
+++ PPYr10/main.py
@@ -1,9 +1,21 @@
-rows = [[0, 0, 0, 0], [0, 2, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+import os
+import random 
+from colorama import init, Fore, Style
+import time
 
+rows = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
+Yellow_start = Fore.YELLOW + Style.BRIGHT
 
+Cyan_start = Fore.CYAN + Style.BRIGHT
+
+green_start = Fore.GREEN + Style.BRIGHT
+
+Purple_start = Fore.MAGENTA + Style.BRIGHT
+
+Colour_end = Style.RESET_ALL
 def tutorial():
-    print("How to play: \nUse WASD to merge identical blocks\nIdentical blocks will add together to form a larger block (eg 2→2 = 4)\nEvery movement will spawn either a 2 or a 4 on a random empty space\nKeep playing until you creat 2048 or run out of space!")
+    print("How to play: \nUse ",Yellow_start, "WASD", Colour_end ," to merge identical blocks\nIdentical blocks will add together to form a larger block (eg 2→2 = 4)\nEvery movement will spawn either a 2 or a 4 on a random empty space\nKeep playing until you create", Yellow_start, "2048", Colour_end," or run out of space!")
 
 def display_board():
     largestValue = rows[0][0]
@@ -23,8 +35,28 @@ def display_board():
                 currentRow += (" " * (Spaces - len(str(block)))) + str(block) + "|"
         print(currentRow)
 
+def MergeOneRowLeft(row):
+
+    for i in range(rows - 1, 0, -1):
+        if row[i-1] == 0:
+            row[i-1] = row[i]
+            row[i] = 0
+    
+    for i in range(rows-1):
+        if row[i] == row[i+1]:
+            row[i] *= 2
+            row[i+1] = 0
+
+    for i in range(rows - 1, 0, -1):
+        if row[i-1] == 0:
+            row[i-1] = row[i]
+            row[i] = 0
+
+    return row
+    
 def MergeLeft():
-    pass
+    for row in rows:
+        MergeOneRowLeft()
 
 def MergeRight():
     pass
@@ -35,35 +67,48 @@ def MergeUp():
 def MergeDown():
     pass
 
+def updateScore():
+    pass
 
 def scoreboard():
-    Scoreboard = open("scores.txt", "w")
+    Scoreboard = open("Leaderboard.txt", "w")
     Scoreboard.close()
     return Scoreboard
 
-def IsFileEmpty():
-    pass
+def IsFileEmpty(Scoreboard):
+    if not os.path.exists(Scoreboard):
+        print("Sorry! No one has played yet!")
+        return False
+    
+    else:
+        with open(Scoreboard, 'r') as file:
+            leaderboard = file.read()
+            print(leaderboard)
+    
+    return os.path.getsize(Scoreboard) == 0
 
+def Game():
+    print("Work in progress")
 
 
 def main_menu():
-    print("Welcome to 2048.\n[Play]\n[Tutorial]\n[Score]")
+    print(Yellow_start,"Welcome to...")
+    time.sleep(1)
+    print("██████╗░░█████╗░░░██╗██╗░█████╗░\n╚════██╗██╔══██╗░██╔╝██║██╔══██╗\n░░███╔═╝██║░░██║██╔╝░██║╚█████╔╝\n██╔══╝░░██║░░██║███████║██╔══██╗\n███████╗╚█████╔╝╚════██║╚█████╔╝\n╚══════╝░╚════╝░░░░░░╚═╝░╚════╝░",Colour_end)
+    time.sleep(1)
+    print(green_start,"[Play]",Colour_end)
+    time.sleep(0.5)
+    print(Cyan_start,"[Tutorial]", Colour_end)
+    time.sleep(0.5)
+    print(Purple_start,"[Score]",Colour_end)
+    time.sleep(0.5)
     choice = input("What would you like to do?\n")
+
     if choice == "Tutorial":
         tutorial()
     elif choice == "Score":
         pass
+
     elif choice == "Play":
-        display_board()
+        Game()
 
-
-
-def scoreboard():
-    Scoreboard = open("scores.txt", "w")
-    Scoreboard.close()
-    return Scoreboard
-
-def updateScore():
-    pass
-
-main_menu()
