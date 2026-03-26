4f495e6cb97958601322b805febee786909a2eeb
Helen <Hmccreery28@kellettschool.com>
2026-03-26 11:15:30 +0800

    Finished the merging left only

--- PPYr10/main.py
+++ PPYr10/main.py
@@ -1,9 +1,11 @@
 import os
-import random 
-from colorama import init, Fore, Style
+from colorama import Fore, Style
 import time
+import sys
 
-rows = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+rows = [[0, 0, 2048, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+Board_size = 4
 
 Yellow_start = Fore.YELLOW + Style.BRIGHT
 
@@ -14,6 +16,8 @@ green_start = Fore.GREEN + Style.BRIGHT
 Purple_start = Fore.MAGENTA + Style.BRIGHT
 
 Colour_end = Style.RESET_ALL
+
+
 def tutorial():
     print("How to play: \nUse ",Yellow_start, "WASD", Colour_end ," to merge identical blocks\nIdentical blocks will add together to form a larger block (eg 2→2 = 4)\nEvery movement will spawn either a 2 or a 4 on a random empty space\nKeep playing until you create", Yellow_start, "2048", Colour_end," or run out of space!")
 
@@ -36,44 +40,59 @@ def display_board():
         print(currentRow)
 
 def MergeOneRowLeft(row):
-
-    for i in range(rows - 1, 0, -1):
-        if row[i-1] == 0:
-            row[i-1] = row[i]
-            row[i] = 0
+    for x in range(Board_size - 1):
+        for i in range(Board_size - 1, 0, -1):
+            if row[i-1] == 0:
+                row[i-1] = row[i]
+                row[i] = 0
     
-    for i in range(rows-1):
+    for i in range(Board_size-1):
         if row[i] == row[i+1]:
             row[i] *= 2
             row[i+1] = 0
 
-    for i in range(rows - 1, 0, -1):
+    for i in range(Board_size - 1, 0, -1):
         if row[i-1] == 0:
             row[i-1] = row[i]
             row[i] = 0
 
     return row
     
-def MergeLeft():
-    for row in rows:
-        MergeOneRowLeft()
+def MergeLeft(New_Board):
+    for i in range(Board_size):
+        New_Board[i] = MergeOneRowLeft(New_Board[i])
+    return New_Board
+
+
+def MergeOneRowRight(row):
+    pass
 
 def MergeRight():
+    pass
+
+def MergeOneRowUp():
     pass
 
 def MergeUp():
     pass
 
+def MergeOneRowDown():
+    pass
+
 def MergeDown():
     pass
 
-def updateScore():
-    pass
+
+
 
 def scoreboard():
     Scoreboard = open("Leaderboard.txt", "w")
     Scoreboard.close()
     return Scoreboard
+
+def updateScore():
+    username = input("Enter name:")
+
 
 def IsFileEmpty(Scoreboard):
     if not os.path.exists(Scoreboard):
@@ -91,11 +110,18 @@ def Game():
     print("Work in progress")
 
 
+
 def main_menu():
     print(Yellow_start,"Welcome to...")
-    time.sleep(1)
-    print("██████╗░░█████╗░░░██╗██╗░█████╗░\n╚════██╗██╔══██╗░██╔╝██║██╔══██╗\n░░███╔═╝██║░░██║██╔╝░██║╚█████╔╝\n██╔══╝░░██║░░██║███████║██╔══██╗\n███████╗╚█████╔╝╚════██║╚█████╔╝\n╚══════╝░╚════╝░░░░░░╚═╝░╚════╝░",Colour_end)
-    time.sleep(1)
+    time.sleep(0.6)
+    print("""
+╔═══╦═══╦╗─╔╦═══╗
+║╔═╗║╔═╗║║─║║╔═╗║
+╚╝╔╝║║║║║╚═╝║╚═╝║
+╔═╝╔╣║║║╠══╗║╔═╗║
+║║╚═╣╚═╝║──║║╚═╝║
+╚═══╩═══╝──╚╩═══╝""",Colour_end)
+    time.sleep(0.6)
     print(green_start,"[Play]",Colour_end)
     time.sleep(0.5)
     print(Cyan_start,"[Tutorial]", Colour_end)
@@ -112,3 +138,5 @@ def main_menu():
     elif choice == "Play":
         Game()
 
+
+main_menu()
